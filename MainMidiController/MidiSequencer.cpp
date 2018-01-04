5a09feb8b068c1e3b7813eda291daae531f3236f
Thomas Jung <jut@bbtsoftware.ch>
2018-01-04 08:37:07 +0100

    WIP

--- MainMidiController/MidiSequencer.cpp
+++ MainMidiController/MidiSequencer.cpp
@@ -3,6 +3,13 @@
 // 
 
 #include "MidiSequencer.h"
+#define MIDISERIAL Serial1	//RX 0, TX 1 -> MIDI
+
+MidiSequencer::MidiSequencer(int midiInPin, int midiOutPin){
+    _midiInPin = midiInPin;
+    _midiOutPin = midiOutPin;
+    
+}
 
 void MidiSequencerClass::init()
 {
--- MainMidiController/MidiSequencer.h
+++ MainMidiController/MidiSequencer.h
@@ -3,19 +3,34 @@
 #ifndef _MIDISEQUENCER_h
 #define _MIDISEQUENCER_h
 
+/*
 #if defined(ARDUINO) && ARDUINO >= 100
 	#include "wprogram.h"
 #else
 	#include "WProgram.h"
 #endif
+*/
 
 class MidiSequencerClass
 {
  protected:
-
+private:
+//int _midiInPin;
+//int _midiOutPin;
+int _bpm;
+int _notes[64];
+int _volumes[64];
 
  public:
-	void init();
+ MidiSequencer();
+ void start();
+ void stop();
+ void setBpm(int bpm);
+ int *getVolumes(int values[64]);
+ int *getNotes()int notes[64]);
+ void setVolumes(int values[64]);
+ void setNotes(int notes[64]);
+	//void init();
 };
 
 extern MidiSequencerClass MidiSequencer;
